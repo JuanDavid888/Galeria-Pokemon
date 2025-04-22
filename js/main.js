3588e41dd1bfc0b464e699b4a1e23bf51f559e06
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-04-22 16:10:28 -0500

    feat: :lipstick: Se agregó una opción de eliminar al darle al boton rojo

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ const namePoke = document.getElementById('namePoke')
 const imgPoke = document.getElementById('imgPoke')
 const addPoke = document.getElementById('addPoke')
 
-addPoke.addEventListener('click', (e)=>{
-    e.preventDefault()
-    console.log(namePoke.value);
-    
+function agregarPoke() {
     const card = document.createElement('div')
     card.classList.add('card')
     card.innerHTML = `
-    <button class="delete">X</button>
+    <button id="remove" class="delete">X</button>
     <img class="img" src="${imgPoke.value}" alt="${namePoke.value}">
     <h2 class="name">${namePoke.value}</h2>`
     document.getElementById('list').appendChild(card)
-})
+    
+    const removeButton = card.querySelector('.delete');
+    removeButton.addEventListener('click', function() {
+        card.remove();
+    });
+}
+addPoke.addEventListener('click', agregarPoke)
